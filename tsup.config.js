c9704f69f2d76371299968589232e727125cba16
Chukwuma Nwaugha <nwaughac@gmail.com>
2021-12-22 20:18:28 +0400

    minify the output for an even small bundle

--- tsup.config.js
+++ tsup.config.js
@@ -3,6 +3,7 @@ import {defineConfig} from 'tsup'
 export default defineConfig({
   sourcemap: true,
   clean: true,
+  minify: true,
   entryPoints: ['index.ts'],
   format: ['esm', 'cjs'],
   outDir: 'lib',
