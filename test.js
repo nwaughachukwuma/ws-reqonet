fa84cbb382ad3aaa615b4bf2b78b18b58272c3ae
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-01-08 00:26:45 +0400

    mock all the events emitted by WSRekanet

--- test.js
+++ test.js
@@ -17,12 +17,61 @@ class TestApp {
   sendMessage(message) {
     this.connection.send(message);
   }
+
+  get instance() {
+    return this.connection;
+  }
 }
 
 const FAKE_URL = "ws://localhost:8080";
 const SERVER_RESPONSE = "test message from mock server";
 const CLIENT_MESSAGE = "test message from app";
 const WAIT_TIME = 100;
+
+test.serial("WSRekanet can listen on open connection", async (t) => {
+  const mockServer = new Server(FAKE_URL);
+  mockServer.on("connection", () => {});
+
+  const app = new TestApp(FAKE_URL);
+  app.instance.on("open", () => {
+    t.pass();
+  });
+
+  await sleep(WAIT_TIME);
+  mockServer.stop(t.done);
+});
+
+test.serial("WSRekanet can listen on close connection", async (t) => {
+  const mockServer = new Server(FAKE_URL);
+  mockServer.on("connection", (socket) => {
+    socket.close();
+  });
+
+  const app = new TestApp(FAKE_URL);
+  app.instance.on("close", () => {
+    t.pass();
+  });
+
+  await sleep(WAIT_TIME);
+  mockServer.stop(t.done);
+});
+
+test.serial("WSRekanet can listen on errored connection", async (t) => {
+  const DIFF_FAKE_URL = "ws://localhost:8080/diff";
+
+  const mockServer = new Server(DIFF_FAKE_URL);
+  mockServer.on("connection", (socket) => {
+    socket.close();
+  });
+
+  const app = new TestApp(FAKE_URL);
+  app.instance.on("error", (error) => {
+    t.is(error.type, "error");
+  });
+
+  await sleep(WAIT_TIME);
+  mockServer.stop(t.done);
+});
 
 test.serial("websocket client can receive message from server", async (t) => {
   const mockServer = new Server(FAKE_URL);
