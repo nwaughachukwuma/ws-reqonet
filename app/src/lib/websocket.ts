c05bffec6ba1acad9002bff46312f61f21d7583a
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-01-09 15:47:24 +0400

    update enableLogging option in test and dev app

--- app/src/lib/websocket.ts
+++ app/src/lib/websocket.ts
@@ -3,7 +3,7 @@ import WSRekanet from '../../../lib/index.js';
 const API_URL = 'http://localhost:8080';
 export default function websocket(): WSRekanet {
 	const SERVER_URL = `${API_URL.replace(/http/, 'ws')}/ws/`;
-	return new WSRekanet(SERVER_URL);
+	return new WSRekanet(SERVER_URL, [], { enableLogging: true });
 }
 
 export { WSRekanet as Websocket };
