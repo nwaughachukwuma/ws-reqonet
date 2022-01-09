7d4039b85373d4c2f5ca79df3f5c5f6b562a1f18
Chukwuma Nwaugha <nwaughac@gmail.com>
2022-01-09 22:13:11 +0400

    use npmjs/ws-rekanet in app

--- app/src/lib/websocket.ts
+++ app/src/lib/websocket.ts
@@ -1,4 +1,5 @@
-import WSRekanet from '../../../lib/index.js';
+// import WSRekanet from '../../../lib/index.js';
+import WSRekanet from 'ws-rekanet';
 
 const API_URL = 'http://localhost:8080';
 export default function websocket(): WSRekanet {
