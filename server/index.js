8d733542db3650f0de82cc08685b821da4075de2
Chukwuma Nwaugha <nwaughac@gmail.com>
2023-02-17 00:27:39 +0400

    update fastify related deps

--- server/index.js
+++ server/index.js
@@ -1,33 +1,37 @@
-import fastify from 'fastify'
-import cors from 'fastify-cors'
-import fastifyWS from 'fastify-websocket'
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import fastifyWS from "@fastify/websocket";
 
-const PORT = 8080
-const ADDRESS = '0.0.0.0'
+const PORT = 8080;
+const ADDRESS = "0.0.0.0";
 const getEnvDetails = () => ({
-    VERSION: process.env.VERSION || 'local',
-    PORT,
-})
+  VERSION: process.env.VERSION || "local",
+  PORT,
+});
 
 fastify()
-    .register(cors, { origin: true })
-    .register(fastifyWS)
-    .setErrorHandler((error, _req, reply) => {
-      console.log('error', error.toString())
-      reply.send({ error: 'Something went wrong' })
-    })
-    .get('/', async () => getEnvDetails())
-    .get('/ws/*', { websocket: true }, ({ socket }, req) => {
-      socket.on('message', (msg) => {
-        console.log('from client', msg.toString(), req.url)
-        const res = JSON.stringify({
-          pong: `acked: ${Date.now()}`,
-          msg: `${msg}`,
-        })
-        socket.send(res)
-      })
-    })
-    .listen(PORT, ADDRESS)
-    .then((v) => {
-      console.log(`ws server started on ${v}`)
-    })
+  .register(cors, { origin: true })
+  .register(fastifyWS)
+  .setErrorHandler((error, _req, reply) => {
+    console.log("error", error.toString());
+    reply.send({ error: "Something went wrong" });
+  })
+  .setNotFoundHandler((req, reply) => {
+    console.log("not found", req.url);
+    reply.status(404).send({ error: "Not found" });
+  })
+  .get("/", async () => getEnvDetails())
+  .get("/ws/*", { websocket: true }, ({ socket }, req) => {
+    socket.on("message", (msg) => {
+      console.log("from client", msg.toString(), req.url);
+      const res = JSON.stringify({
+        pong: `acked: ${Date.now()}`,
+        msg: `${msg}`,
+      });
+      socket.send(res);
+    });
+  })
+  .listen({ port: PORT, host: ADDRESS })
+  .then((v) => {
+    console.log(`ws server started on ${v}`);
+  });
